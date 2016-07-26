2966d7d41e5057c71a68e1d208ee6481052fafae
SruthiRagavan <sruthiragavan14@gmail.com>
2016-07-26 18:29:17 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,19 @@
+package wiproprog;
+
+import java.util.Scanner;
+
+public class Length 
+{
+public static void main(String[] args) 
+    {
+	String str;
+	int n;
+	System.out.println("enter string");
+	Scanner sc = new Scanner(System.in);
+	str=sc.nextLine();
+	n=str.length();
+	System.out.println(n);
+
+	}
+
+}
